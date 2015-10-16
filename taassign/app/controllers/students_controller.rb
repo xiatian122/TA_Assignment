bd49ec930c6c7bbaa94ea0dbf801b377922cb64b
Wei Zhao <wei.zhao@tamu.edu>
2015-10-16 04:29:33 +0000

    add new student finished

--- taassign/app/controllers/students_controller.rb
+++ taassign/app/controllers/students_controller.rb
@@ -3,5 +3,14 @@ class StudentsController < ApplicationController
         @students = Student.all
     end
     
+    def new
+    # default: render 'new' template
+    end
     
+    def create
+        @student = Student.create!(params[:student])
+        #debugger
+        flash[:notice] = "#{@student.uin} was successfully created."
+        redirect_to students_path
+    end
 end
