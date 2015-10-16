4f979b8c37b7128832347a14c0d6a7317271b4b9
iamyangchen <iamyangchen@gmail.com>
2015-10-15 23:41:45 -0500

    Added course model

--- taassign/config/routes.rb
+++ taassign/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :students
+  resources :courses
   #root :to => redirect('/students')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
