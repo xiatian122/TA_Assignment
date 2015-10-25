8ebe89c8438038cc4fb8454e60b567fc2d89b436
bovetliu <bowei_liu@ymail.com>
2015-10-25 11:33:41 -0500

    basic actions for controller Course is there

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,4 +2,47 @@ class CoursesController < ApplicationController
 	def index
     @courses = Course.all
   end
+
+  #  /courses/new
+  def new
+  # default: render 'new' template
+    @course = Course.new
+  end
+
+
+  # POSt /courses
+  def create
+    @course = Course.create!(params[:student])
+    #debugger
+    flash[:notice] = "#{@course.name} was successfully created."
+    redirect_to courses_path
+    end
+
+  # GET /courses/:id
+  def show
+    @course = Course.find(params[:id])
+  end
+  
+  # GET /courses/:id/edit
+  def edit
+    @course = Course.find params[:id]
+  end
+  
+
+  # PATCH /courses/:id
+  def update
+    @course = Course.find params[:id]
+    @course.update_attributes!(params[:student])
+    flash[:notice] = "#{@course.name} was successfully updated."
+    redirect_to course_path(@course)
+  end
+
+  # DELETE /courses/:id
+  def destroy
+    @course = Course.find(params[:id])
+    respond_to do |format|
+      format.html {redirect_to courses_url, notice: "Course #{@course.name} was successfully destroyed"}
+      format.json {head :no_content}
+    end
+  end
 end
