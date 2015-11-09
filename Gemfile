51dd720bd22406bd839ccc79761499b199951d1c
iamyangchen <yangchen08@tamu.edu>
2015-11-08 23:27:22 -0600

    added application_pool model and corresponded controller and views

--- Gemfile
+++ Gemfile
@@ -39,6 +39,10 @@ gem 'haml'
 gem 'protected_attributes'
 # to use attr_accessible
 
+gem 'bootstrap-datepicker-rails'
+gem 'momentjs-rails', '>= 2.9.0'
+gem 'bootstrap3-datetimepicker-rails', '~> 4.17.37'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151108205936) do
+ActiveRecord::Schema.define(version: 20151109041126) do
+
+  create_table "application_pools", force: :cascade do |t|
+    t.string   "year"
+    t.string   "semester"
+    t.datetime "deadline"
+    t.boolean  "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string   "cid"
