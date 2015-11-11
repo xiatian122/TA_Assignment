c6abbe221b42333548e222a7e7c9949f85c4f20a
iamyangchen <yangchen08@tamu.edu>
2015-11-10 20:28:29 -0600

    some modification

--- app/models/app_course_matching.rb
+++ app/models/app_course_matching.rb
@@ -1,15 +1,12 @@
 class AppCourseMatching < ActiveRecord::Base
 	attr_accessible :application_pool_id, :student_application_id, :course_id, :position, :status
 	
-	UNDER_REVIEW = 0
-	LECTURER_REQUEST = 1
-  TEMP_ASSIGNED = 2
-  EMAIL_NOTIFIED = 3
-  STUDENT_CONFIRMED = 4
-  STUDENT_REJECTED = 5
-  ASSIGNED = 6
+  TEMP_ASSIGNED = 1
+  EMAIL_NOTIFIED = 2
+  STUDENT_CONFIRMED = 3
+  STUDENT_REJECTED = 4
+  ASSIGNED = 5
 
-  NOPOSITION = 0
   FULLTA = 1
   HALFTA = 2
   GRADER = 3
