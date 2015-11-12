3e3299b0a08ca02b00e35205260367bb63a115e6
bovetliu <bowei_liu@ymail.com>
2015-11-12 00:00:48 -0600

    now when back from add TA, the accordian will expand automatically

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -2,11 +2,23 @@
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
+
+// following code is to prevent course row from expanding when user clicked edit button of each course row at courses index 
 $(document).ready(function (){
   $('.edit-op-btn').click(function(e){
     e.preventDefault();
     e.stopPropagation();
     window.open($(this).attr('href'));
-    
   });
+
+  window.setTimeout(function delayed(){
+    if (window.location.hash){
+      var anchor = window.location.hash;
+      anchor = anchor.replace("heading","collapse");
+      console.log(anchor);  // #heading1
+      $(anchor).collapse("show");
+    }
+  },500);
+
+
 });
