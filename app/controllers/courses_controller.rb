d1437bd469a7353233146d6fe6fad8601c6f4798
minjielu <minjielu199035@gmail.com>
2015-11-15 04:03:04 +0000

    user-request-3

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -154,14 +154,6 @@ class CoursesController < ApplicationController
       end
       @student_application_info[studentapplication.id] = info_for_student
     end
-    tasuggestion = @course.suggestion.split('/')
-    tasuggestion.each do |ta_info|
-      if @suggestion != nil
-        @suggestion << '/' + ta_info.split(';')[0]
-      else
-        @suggestion = ta_info.split(';')[0]
-      end
-    end
     
   end
 
